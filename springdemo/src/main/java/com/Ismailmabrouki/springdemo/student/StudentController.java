5329fa7084037f91c82336bb70eff9629fb392e6
Ismail Mabrouki <39934485+IsmailMabrouki@users.noreply.github.com>
2024-02-11 22:54:54 +0100

    implementing service layer

--- springdemo/src/main/java/com/Ismailmabrouki/springdemo/student/StudentController.java
+++ springdemo/src/main/java/com/Ismailmabrouki/springdemo/student/StudentController.java
@@ -1,20 +1,20 @@
 package com.Ismailmabrouki.springdemo.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
+
 public class StudentController {
 
     private StudentService service;
-
     public StudentController(StudentService service) {
         this.service = service;
     }
